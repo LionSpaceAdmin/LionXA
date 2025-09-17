d26cf35212938e649223ccf923b79094a8ee63bc
דניאל חנוכייב <Hanudani@gmail.com>
2025-09-17 22:07:06 +0000

    Try fixing this error: `Runtime Error: Error: Couldn't find next-intl co

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,6 @@
 import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!['en', 'he'].includes(locale as any)) {
-      // Instead of throwing an error, you can define a default locale.
-      // For this example, let's stick to throwing an error.
-      throw new Error(`Invalid locale: ${locale}`);
-  }
- 
   return {
     messages: (await import(`../messages/${locale}.json`)).default
   };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,26 @@
 import createMiddleware from 'next-intl/middleware';
+ 
+export const locales = ['he', 'en'];
+export const defaultLocale = 'he';
+export const localePrefix = 'always';
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['he', 'en'],
+  locales,
  
   // Used when no locale matches
-  defaultLocale: 'he'
+  defaultLocale,
+
+  // Always show the locale prefix
+  localePrefix
 });
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(he|en)/:path*']
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    '/((?!api|_next|_vercel|.*\\..*).*)',
+  ],
 };
